38a380c89fa4ea843d6dcb27bd448044fe58ef22
Destin Meza <desti@Maple.DESKTOP-J4NQENH>
2020-04-20 14:26:27 -0500

    Added basic animations to plane movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraController : MonoBehaviour
+{
+    public float speed = 100;
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 1, -20);
+
+    void FixedUpdate()
+    {
+        if(target == null) return;
+
+        if(Application.isPlaying){
+            transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.fixedDeltaTime * speed);
+        }
+        
+
+        else{
+            transform.position = target.position + offset;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public Vector3 speed = new Vector3(30,-20, 100);
+    public float maxSpeedChange = 10;
+    Vector3 targetVelocity;
+    Rigidbody rb;
+    Animator anim;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update(){
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        bool shift = Input.GetKey(KeyCode.LeftShift);
+        if(shift) x *= 2;
+
+
+        targetVelocity = new Vector3(x, y, 0).normalized + Vector3.forward;
+        targetVelocity = Vector3.Scale(targetVelocity, speed);
+        
+        anim.SetFloat("xVel", x);
+        anim.SetFloat("yVel", -y);
+    }
+
+    void FixedUpdate(){
+
+        Vector3 velocityChange = targetVelocity - rb.velocity;
+        Vector3 xyChange = velocityChange;
+        xyChange.z = 0;
+        if(xyChange.sqrMagnitude > maxSpeedChange * maxSpeedChange){
+            xyChange = xyChange.normalized * maxSpeedChange;
+            velocityChange.x = xyChange.x;
+            velocityChange.y = xyChange.y;
+        }
+        rb.AddForce(velocityChange, ForceMode.VelocityChange);
+    }
+}
