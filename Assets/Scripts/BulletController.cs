107562eba347698d8ae47fa8fcb09706fe7c06d5
Destin Meza <desti@Maple.DESKTOP-J4NQENH>
2020-04-20 18:55:45 -0500

    added normal maps and a nice effects to make the model look better

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed = 250;
+    public float bulletLifetime = 5;
+    public float startTime = 0;
+    Rigidbody rb;
+    void Awake(){
+        rb = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        rb.velocity = Vector3.forward * speed;
+    }
+
+    void Update(){
+        if(Time.time - startTime > bulletLifetime){
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -6,40 +6,82 @@ public class PlayerControler : MonoBehaviour
 {
     public Vector3 speed = new Vector3(30,-20, 100);
     public float maxSpeedChange = 10;
+    public GameObject bulletPrefab;
+    public int maxBullets = 14;
+    public Transform firePos1;
+    public Transform firePos2;
+    BulletController[] bullets;
     Vector3 targetVelocity;
     Rigidbody rb;
     Animator anim;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
     }
 
+    void Start(){
+        bullets = new BulletController[maxBullets];
+        for(int i = 0; i < maxBullets; i++){
+            GameObject bullet = Instantiate(bulletPrefab);
+            bullets[i] = bullet.GetComponent<BulletController>();
+            bullet.gameObject.SetActive(false);
+        }
+    }
+
     void Update(){
+        Movement();
+        if(Input.GetButtonDown("Submit")){
+            Fire();
+        }
+        
+    }
+    void FixedUpdate(){
+
+            Vector3 velocityChange = targetVelocity - rb.velocity;
+            Vector3 xyChange = velocityChange;
+            xyChange.z = 0;
+            if(xyChange.sqrMagnitude > maxSpeedChange * maxSpeedChange){
+                xyChange = xyChange.normalized * maxSpeedChange;
+                velocityChange.x = xyChange.x;
+                velocityChange.y = xyChange.y;
+            }
+            rb.AddForce(velocityChange, ForceMode.VelocityChange);
+    }
+    void Movement(){
 
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         bool shift = Input.GetKey(KeyCode.LeftShift);
-        if(shift) x *= 2;
 
+        if(shift && Mathf.Abs(x) < 2){
+            x *= 2;
+        }
 
         targetVelocity = new Vector3(x, y, 0).normalized + Vector3.forward;
         targetVelocity = Vector3.Scale(targetVelocity, speed);
-        
+
         anim.SetFloat("xVel", x);
         anim.SetFloat("yVel", -y);
     }
 
-    void FixedUpdate(){
-
-        Vector3 velocityChange = targetVelocity - rb.velocity;
-        Vector3 xyChange = velocityChange;
-        xyChange.z = 0;
-        if(xyChange.sqrMagnitude > maxSpeedChange * maxSpeedChange){
-            xyChange = xyChange.normalized * maxSpeedChange;
-            velocityChange.x = xyChange.x;
-            velocityChange.y = xyChange.y;
+    void Fire(){
+        for(int i = 0; i < bullets.Length; i++){
+            if(!bullets[i].gameObject.activeSelf){
+                bullets[i].gameObject.SetActive(true);
+                bullets[i].startTime = Time.time;
+                bullets[i].gameObject.transform.position = firePos1.position;
+                break;
+            }
         }
-        rb.AddForce(velocityChange, ForceMode.VelocityChange);
+        for(int i = 0; i < bullets.Length; i++){
+            if(!bullets[i].gameObject.activeSelf){
+                bullets[i].gameObject.SetActive(true);
+                bullets[i].startTime = Time.time;
+                bullets[i].gameObject.transform.position = firePos2.position;
+                return;
+            }
+        }
     }
 }
