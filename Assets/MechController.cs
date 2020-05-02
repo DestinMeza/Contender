93c471c39f8d24c38e41e88c90f0df070ce6e49e
Destin Meza <desti@Maple.DESKTOP-J4NQENH>
2020-05-02 14:03:22 -0500

    Added mech to the game and created some basic animations for the mech, Also a movement script

---
+++ Assets/MechController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechController : MonoBehaviour
+{
+    public float moveForce = 5; 
+    Rigidbody rb;
+    void Awake(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void MoveForward(){
+        rb.AddForce(transform.forward * moveForce, ForceMode.Impulse);
+    }
+}
