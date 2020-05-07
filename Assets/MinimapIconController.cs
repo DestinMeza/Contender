a784f802baa73a4300bac59f6f28aeaeb41dff0f
Destin Meza <desti@Maple.DESKTOP-J4NQENH>
2020-05-07 14:31:05 -0500

    Added mini map to the game and mini map icons

---
+++ Assets/MinimapIconController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapIconController : MonoBehaviour
+{
+    Transform parentPos;
+    void Awake(){
+        parentPos = GetComponentInParent<Transform>();
+    }
+    void LateUpdate()
+    {
+        transform.forward = parentPos.forward;
+    }
+}
---
+++ Assets/Scripts/HealthRingController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthRingController : MonoBehaviour
+{
+    public int health = 5;
+    void OnTriggerEnter(Collider col){
+        PlayerControler player = col.GetComponentInParent<PlayerControler>();
+        if(player != null){
+            player.GetComponentInParent<HealthController>().health += health;
+        }
+    }
+}
