52adc5d7ef05c70f188ea82174f97a289cc6adc6
Destin Meza <desti@Maple.DESKTOP-J4NQENH>
2020-05-15 11:49:26 -0500

    Added body part colliders to the dragon's wings and body.

---
+++ Assets/BossFightController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFightController : MonoBehaviour
+{
+    public string bossName;
+    void OnTriggerEnter(Collider col){
+        if(col.GetComponentInParent<PlayerController>()){
+            SpawnManager.Spawn(bossName, transform.position);
+        }
+    }
+}
