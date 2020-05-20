c08d96607c9ae8c4d9ff36a26fc991186a885fa5
Destin Meza <desti@Maple.DESKTOP-J4NQENH>
2020-05-20 11:33:04 -0500

    added animation states and 3 different states to the boss movement. Added a tighter turn for it's turning state

--- Assets/BossMovementController.cs
+++ Assets/BossMovementController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class BossMovementController : MonoBehaviour
 {
+    enum BossState{
+        Flying,
+        Gliding, 
+        Turning
+    }
+    BossState bossState = BossState.Flying;
+    Animator anim;
     public LayerMask obsticles;
     public float rotationalDamp = 1;
     public float speed = 60;
@@ -11,14 +18,50 @@ public class BossMovementController : MonoBehaviour
     Rigidbody rb;
 
     void Awake(){
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
     }
 
     void Update(){
+        
+        if(bossState == BossState.Flying)Flying();
+        if(bossState == BossState.Turning)Turning();
+        if(bossState == BossState.Gliding)Gliding();
+        CheckObsticles();
+        ClampPos();
+    }
+
+    void Gliding(){
+
+    }
+
+    void Turning(){
         Vector3 diff = PlayerController.player.transform.position - transform.position;
         Quaternion rotation = Quaternion.LookRotation(diff);
         rotation.z = 0;
         rotation.x = Mathf.Clamp(rotation.x, -5, 5);
+        anim.SetFloat("horizontalTurn", rotation.x);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationalDamp * Time.deltaTime);
+        
+        rb.AddForce(transform.forward * speed, ForceMode.VelocityChange);
+        rb.velocity = rb.velocity = new Vector3(
+            Mathf.Clamp(rb.velocity.x, speed*-0.5f, speed*0.5f),
+            Mathf.Clamp(rb.velocity.y, speed*-0.5f, speed*0.5f),
+            Mathf.Clamp(rb.velocity.z, speed*-0.5f, speed*0.5f)
+        );
+
+        anim.SetInteger("AnimState", (int)bossState);
+        if(Vector3.Dot(transform.forward, PlayerController.player.transform.forward) > 0){
+            bossState = BossState.Flying;
+        }
+    }
+
+    void Flying(){
+        Vector3 diff = PlayerController.player.transform.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(diff);
+        rotation.z = 0;
+        rotation.x = Mathf.Clamp(rotation.x, -5, 5);
+        anim.SetFloat("horizontalTurn", rotation.x);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationalDamp * Time.deltaTime);
         
         rb.AddForce(transform.forward * speed, ForceMode.VelocityChange);
@@ -29,16 +72,13 @@ public class BossMovementController : MonoBehaviour
         );
 
         Vector3 headDiff = PlayerController.player.transform.position - transform.position;
-
+        anim.SetInteger("AnimState", (int)bossState);
         if(Vector3.Dot(transform.forward, PlayerController.player.transform.forward) < 0.3){
             head.up = headDiff.normalized;
         }
         else{
-            head.up = -1 * transform.forward;
+            bossState = BossState.Turning;
         }
-
-        CheckObsticles();
-        ClampPos();
     }
 
     void CheckObsticles(){
@@ -50,6 +90,6 @@ public class BossMovementController : MonoBehaviour
     }
 
     void ClampPos(){
-        transform.position = new Vector3 (transform.position.x, Mathf.Clamp(transform.position.y, 0, 300), transform.position.z) ;
+        transform.position = new Vector3 (transform.position.x, Mathf.Clamp(transform.position.y, 20, 300), transform.position.z) ;
     }
 }
