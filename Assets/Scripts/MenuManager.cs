afb6646b9270892769f216d12fdd34a3a98caa43
Destin Meza <desti@Maple.DESKTOP-J4NQENH>
2020-05-27 15:23:37 -0500

    Added menu mission select option, and added animations.

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
+    enum MenuScreens{
+        MainMenu,
+        PlayMenu
+    }
+    MenuScreens menuScreens = MenuScreens.MainMenu;
+    Animator anim;
+
+    void Awake(){
+        anim = GetComponent<Animator>();
+    }
     void Update(){
-        if(Input.GetButtonDown("Cancel")){
+        if(Input.GetButtonDown("Cancel") && menuScreens == MenuScreens.MainMenu){
             Application.Quit();
         }
+        if(Input.GetButtonDown("Cancel") && menuScreens == MenuScreens.PlayMenu){
+            anim.Play("MainState"); // Scrollanimation
+            menuScreens = MenuScreens.MainMenu;
+        }
+    }
+    public void PlayGame(){
+        anim.Play("PlayState");
+        menuScreens = MenuScreens.PlayMenu;
     }
     public void Tutorial(){
         StartCoroutine(SceneLoader("Tutorial"));
     }
     public void Quit(){
         Application.Quit();
+    }
+
+    public void Mission1(){
+        SceneManager.LoadScene("Mission1");
+    }
+    public void Mission2(){
+        SceneManager.LoadScene("Mission2");
     }
 
     IEnumerator SceneLoader(string sceneToLoad){
